0391f0c574b8e83c28b44ba961e27e7bfbdc32e3
川中潤 <157383282+jun-kawanaka@users.noreply.github.com>
2024-02-14 22:19:46 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+// 体重の宣言と代入
+let bodyWeight;
+bodyWeight = 68;
+console.log(bodyWeight);
+
+// 身長の宣言と代入
+let height;
+height = 1.7;
+console.log(height);
+
+BMIの式と出力結果
+console.log(bodyWeight/(height*height));
+
+// BMI値の宣言と代入
+let BMI;
+BMI = (bodyWeight/(height*height));
+console.log(BMI);
